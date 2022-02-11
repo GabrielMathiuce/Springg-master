bc67c8908c3ef6487bf3fc6b4a34b813cbf5491c
gsilva <gabrmathiuce@gmail.com>
2022-02-11 09:37:51 -0300

    Commit Message

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -78,6 +78,14 @@ public class Order implements Serializable {
     public void setClient(User client) {
         this.client = client;
     }
+    public Double getTotal() {
+        double sum = 0.0;
+        for(OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
+
+        }
 
     @Override
     public boolean equals(Object o) {
